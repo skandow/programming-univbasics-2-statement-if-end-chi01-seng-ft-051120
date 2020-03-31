1038408be68aa3e1a9fb4acaaffa22b668f81792
Steven Kandow <kando1sr@gmail.com>
2020-03-31 15:13:19 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,8 @@
 # Write your solution here
+the_time = Time.now
+the_time_integer = the_time.to_i
+if the_time_integer % 2 == 0 
+  puts "Even!"
+  elsif the_time_integer % 2 == 1 
+  puts "Odd!"
+end 
